4716407734a96dcfb313cf4aae4cf337d5963037
Lorelie Defensor <lddefensor@gmail.com>
2017-02-28 10:59:50 +0800

    Custom Application Handler

---
+++ ApiErrorHandler.php
@@ -0,0 +1,21 @@
+<?php
+
+//returns an invalid json response when encountered an error
+require_once (APP_ROOT . DS . 'Lib' . DS . 'Helpers' . DS. 'MyExceptionHandler.php');
+
+use Ulap\Helpers\MyExceptionHandler as MyExceptionHandler;
+use Ulap\Helpers\MyRuntimeException as MyRuntimeException;
+
+class ApiErrorHandler extends MyExceptionHandler
+{
+	
+	public function handle(MyRuntimeException $exception){
+		$error = array('success'=>false, 'error'=>$exception->getMessage(), 'code'=>$exception->getCode());
+	
+		echo json_encode($error);
+	}
+}
+
+
+
+/** END OF FILE **/

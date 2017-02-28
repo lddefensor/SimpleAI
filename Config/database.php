1e6394dee44003d17f8965dc025c427d3838a06b
Lorelie Defensor <lddefensor@gmail.com>
2017-03-01 07:29:25 +0800

    Config Files

---
+++ Config/database.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+class DBConfig
+{
+	var $default = array(
+		"name" => "redpropertyonline",
+		"host" => "localhost",
+		"user" => "root",
+		"password" => "root",
+		"port" => 8889
+	);   
+	 
+}
+
+/** END OF FILE **/
---
+++ Config/defaults.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once ROOT.DS.'ApiErrorHandler.php';
+
+if(!defined('URL')) 
+	define("URL", '/SimpleAI');  
+
+if(!defined('DEFAULT_ACTION'))
+	define('DEFAULT_ACTION', 'Home');
+
+if(!defined('DEFAULT_METHOD'))
+	define('DEFAULT_METHOD', 'index');
+
+if(!defined('MULTIPLE_APPS'))
+	define('MULTIPLE_APPS', true);
+
+if(!defined('APPS'))
+	define('APPS', array('admin'));
+
+
+
+// END OF FILE 
